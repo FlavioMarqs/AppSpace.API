ee22a62655e1379383412423dd15ba20766838ce
FlavioMarqs <flaviofmmarques@gmail.com>
2023-09-16 17:56:29 +0200

    Added repository project and DAOs

---
+++ src/AppSpace.Repositories/BeezyDbContext.cs
@@ -0,0 +1,28 @@
+﻿using AppSpace.Repositories.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppSpace.Repositories
+{
+    public class BeezyDbContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+        public BeezyDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+           => options.UseSqlServer(_configuration["Databases:BeezyDBConnectionString"]);
+
+        public DbSet<Cinema> Cinemas { get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<MovieGenre> MovieGenres { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Session> Sessions { get; set; }
+    }
+}
---
+++ src/AppSpace.Repositories/Entities/MovieGenre.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppSpace.Repositories.Entities
+{
+    public class MovieGenre
+    {
+        public Movie Movie { get; set; }
+
+        public MovieGenre Genre {get; set;}
+    }
+}
