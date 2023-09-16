a619dd3f95fa945f3a71539dafb85ed7fa23f917
FlavioMarqs <flaviofmmarques@gmail.com>
2023-09-16 15:14:00 +0200

    Adding handlers injection

---
+++ src/AppSpace.Handlers/Extensions/HandlersEx.cs
@@ -0,0 +1,19 @@
+﻿using AppSpace.Handlers.Commands;
+using AppSpace.Handlers.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppSpace.Handlers.Extensions
+{
+    public static class HandlersEx
+    {
+        public static IServiceCollection AddHandlers(this IServiceCollection services)
+        {
+            services.AddTransient<ICommandHandler<SmartBillboardCommand, ISmartBillboardDTO>>();
+
+            return services;
+        }
+    }
+}
---
+++ src/AppSpace.RecommendationsService/Startup.cs
@@ -0,0 +1,30 @@
+﻿using AppSpace.Handlers.Extensions;
+using AppSpace.RecommendationsService.Controllers;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppSpace.RecommendationsService
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddLogging();
+            services.AddHandlers();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            services.AddTransient<RecommendationsController>();
+        }
+    }
+}
